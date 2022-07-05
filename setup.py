15edfe710e03b5f0701eddeb31001ff019d76ac3
noranhe <63988546+noranhe@users.noreply.github.com>
2022-07-05 16:37:02 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,42 +1,4 @@
-import platform
-
-from setuptools import Extension, setup
+from setuptools import setup
 
 
-def get_ext_modules() -> list:
-    """
-    获取三方模块
-
-    Mac由于缺乏二进制库支持无法使用
-    """
-
-    if platform.system() == "Linux":
-        extra_compile_flags = [
-            "-std=c++17",
-            "-O3",
-            "-Wno-delete-incomplete",
-            "-Wno-sign-compare",
-        ]
-        extra_link_args = ["-lstdc++"]
-        runtime_library_dirs = ["$ORIGIN"]
-    else:
-        extra_compile_flags = ["-O2", "-MT"]
-        extra_link_args = []
-        runtime_library_dirs = []
-
-    vnnsq = Extension(
-        name="vnpy_nsq.api.vnnsqmd",
-        sources=["vnpy_nsq/api/vnnsq/vnnsq/vnnsq.cpp"],
-        include_dirs=["vnpy_nsq/api/include", "vnpy_nsq/api/vnnsq"],
-        library_dirs=["vnpy_nsq/api/libs", "vnpy_nsq/api"],
-        libraries=["HSNsqApi"],
-        extra_compile_args=extra_compile_flags,
-        extra_link_args=extra_link_args,
-        runtime_library_dirs=runtime_library_dirs,
-        language="cpp",
-    )
-
-    return [vnnsq]
-
-    
-setup(ext_modules=get_ext_modules())
+setup()
